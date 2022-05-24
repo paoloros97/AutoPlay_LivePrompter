b1d20d255e796b0a274e24b46eb66bb410503311
Paolo <paolorosettani97@gmail.com>
2022-05-24 15:45:33 +0200

    FirstCommit

---
+++ AutoPlayer.py
@@ -0,0 +1,51 @@
+#
+# @Author: Paolo Rosettani
+# @Date: 24/05/2021 (DD/MM/YYY)
+# @Description:
+# This code receive any kind of message received from an external MIDI device (like a Cymatic)
+# Forward the received MIDI message to a loopMIDI channel with an addition MIDI command that trigger
+# the play button on "LivePrompter".
+#
+# In this way it keep simple the MIDI programing for the Cymatic, it just trigger a Program Change
+# matched with a ceratin song (in LivePrompter), and ti peace of code include the play command (send by
+# a MIDI Control Change).
+#
+
+import mido
+import os
+import time
+import sys
+
+outputMIDI='loopMIDI 1' #To LivePrompter
+inputMIDI='emulatore 1' #From Cymatic
+try:
+    outport = mido.open_output(outputMIDI, autoreset=True) #Oper output MIDI port
+    inport = mido.open_input(inputMIDI, autoreset=True) #Open input MIDI port
+
+
+
+    print('Waiting MIDI message from Cymatic...')
+    print()
+    msg = inport.receive() # Leggo il messagio dal Cymatic
+    print('Forward:')
+    print(msg) # Visualizzo il messaggio ricevuto
+    outport.send(msg) # Inoltro il messaggio a Live Prompter
+
+    # Play MIDI command
+    print('Play command:')
+    play = mido.Message('control_change', control=7, value=10) #Play
+    print(play)
+    outport.send(play)
+
+    print() #New line
+
+    time.sleep(1)
+    
+    os.execl(sys.executable, 'python', __file__, *sys.argv[1:]) #Restart program
+
+except:
+    print('Unable to open MIDI port(s).')
+    print('Is the loopMIDI program running?')
+    print('Is the MIDI usb adapter connected?')
+    print('Window closing in 10 seconds...')
+    time.sleep(10)
---
+++ Cymatic.py
@@ -0,0 +1,15 @@
+#
+# @Author: Paolo Rosettani
+# @Date: 24/05/2021 (DD/MM/YYY)
+# @Description:
+# This code is for only testing purpose.
+# Emulates an external device connected via MIDI-usb port.
+#
+
+import mido
+
+outport = mido.open_output('emulatore 2')
+songCymatic = mido.Message('program_change', program=0)
+
+print(songCymatic)
+outport.send(songCymatic)
